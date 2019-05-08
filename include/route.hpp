be0811db2cba0e57aeff7120d446d860984143f2
Bendik Samseth <b.samseth@gmail.com>
2019-05-08 15:09:15 +0200

    Fix index/id issue in test case

--- include/route.hpp
+++ include/route.hpp
@@ -22,38 +22,6 @@ bool          satisfiesConstraints(const Route&, const ProblemInstance&);
 #    include <iostream>
 #    include <random>
 
-TEST_CASE("Constraint check")
-{
-    auto  instance = readDataFile(DATA_FOLDER "/size50.txt");
-    Route route;
-    // clang-format off
-    route.ids = {
-        14, 25, 13, 41, 40, 19, 42, 17,
-        27, 48, 8, 26, 7, 43, 24, 23, 6,
-        38, 9, 30, 34, 50, 21, 29, 16, 11, 32,
-        2, 20, 35, 36, 3, 28, 31, 22, 1,
-        5, 49, 10, 39, 33, 45, 15, 44, 37, 12,
-        46, 47, 4, 18
-    };
-    // clang-format on
-    route.partitions = {8, 17, 27, 36, 46};
-    CHECK(satisfiesConstraints(route, instance) == true);
-
-    // Failing example:
-    // clang-format off
-    route.ids = {
-        14, 25, 13, 41, 40, 19, 42, 17,
-        27, 48, 8, 26, 7, 43, 24, 23, 6, 38,
-        9, 30, 34, 50, 21, 29, 16, 11, 32,
-        2, 20, 35, 36, 3, 28, 31, 22, 1,
-        5, 49, 10, 39, 33, 45, 15, 44, 37, 12,
-        46, 47, 4, 18
-    };
-    // clang-format on
-    route.partitions = {8, 18, 27, 36, 46};
-    CHECK(satisfiesConstraints(route, instance) == false);
-}
-
 TEST_CASE("Random route")
 {
     auto  instance = readDataFile(DATA_FOLDER "/size50.txt");
@@ -64,6 +32,31 @@ TEST_CASE("Random route")
     CHECK(route.partitions.size() > 0);  // Truck can't make just one run for this data.
 
     CHECK(satisfiesConstraints(route, instance) == true);
+}
+
+TEST_CASE("Constraint check")
+{
+    auto  instance = readDataFile(DATA_FOLDER "/size50.txt");
+    Route route;
+    // clang-format off
+    route.ids = {
+         13, 24, 12, 40, 39, 18, 41, 16,
+         26, 47, 7, 25, 6, 42, 23, 22, 5,
+         37, 8, 29, 33, 49, 20, 28, 15, 10, 31,
+         1, 19, 34, 35, 2, 27, 30, 21, 0,
+         4, 48, 9, 38, 32, 44, 14, 43, 36, 11,
+         45, 46, 3, 17
+    };
+    // clang-format on
+    route.partitions = {8, 17, 27, 36, 46};
+    CHECK(satisfiesConstraints(route, instance) == true);
+
+    // Length calculated by hand for this case:
+    CHECK(totalLength(route, instance) == 6180);
+
+    // Failing example: Second tour is to large.
+    route.partitions = {8, 25, 27, 36, 46};
+    CHECK(satisfiesConstraints(route, instance) == false);
 }
 
 #endif
