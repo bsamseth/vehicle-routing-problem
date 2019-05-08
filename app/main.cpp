07eb3cf8da8be1b815fd597de8bd280e7bf2db1f
Bendik Samseth <b.samseth@gmail.com>
2019-05-08 10:48:01 +0200

    Update more boiler plate stuff

--- app/main.cpp
+++ app/main.cpp
@@ -2,8 +2,8 @@
 // doctest definitions. For builds with this disabled, e.g. code shipped to
 // users, this can be left out.
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
-#define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest.h"
+#    define DOCTEST_CONFIG_IMPLEMENT
+#    include "doctest.h"
 #endif
 
 #include <iostream>
@@ -13,5 +13,4 @@
  * Simple main program that demontrates how access
  * CMake definitions (here the version number) from source code.
  */
-int main() {
-}
+int main() {}
